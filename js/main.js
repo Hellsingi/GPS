4eb1b58ee3a51710c13965de598630bedfab5f20
Hellsingi <f.c.realmadrid@mail.ru>
2019-11-25 04:01:13 +0300

    add don't work function change function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
 const modal = document.getElementById("myModal");
 const span = document.getElementsByClassName("close")[0];
-const jobSection = document.querySelector(".job-section");
 const formSection = document.querySelector("form");
+
+const jobSection = document.querySelector(".job-section");
 let selectedDescription;
 
-jobSection.onclick = (event) => {
+// const passImgArray = document.querySelectorAll('.passive');
+// passImgArray.forEach(item => {
+//   let imgS = item.querySelector('img');
+//   imgS.src = imgS.src +'pass.png';
+// })
+let selectedImage;
+const imgSection = document.querySelector(".img-section");
+console.log("imgSection", imgSection);
+imgSection.onclick = event => {
+  const target = event.target;
+  const closestDiv = target.closest("div");
+  if (closestDiv.classList.contains("activeImg")) return;
+  const imsss = document.querySelectorAll('.img-section__item');
+  imsss.forEach(item => {
+    console.log(item);
+    if(item.classList.contains("activeImg")){
+      item.classList.remove('activeImg');
+      let imageDel = item.querySelector('img');
+      console.log('item',item);
+      console.log('imageDel',imageDel);
+      console.log('item.srcitem.src imageDel.src',imageDel.src);
+      imageDel.src = target.src.slice(0, -8) + "pass.png";
+      item.classList.add("passive");
+    }
+  });
+  activeImage(closestDiv, target);
+};
+
+function activeImage(closestDiv, target) {
+  console.log(selectedImage, "selectedImage");
+  closestDiv.classList.remove("passive");
+  closestDiv.classList.add("activeImg");
+  if (selectedImage) {
+    selectedImage.classList.remove("activeImg");
+  }
+  selectedImage = target;
+  selectedImage.src = target.src.slice(0, -8) + "act.png";
+}
+
+jobSection.onclick = event => {
   const target = event.target;
   if (target.tagName === "BUTTON") {
     openModal(target);
@@ -14,6 +54,7 @@ jobSection.onclick = (event) => {
     open(targetDescription);
   }
 };
+
 function open(description) {
   if (description.classList.contains("open")) {
     description.classList.remove("open");
@@ -36,7 +77,7 @@ span.onclick = () => {
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
+window.onclick = event => {
   if (event.target === modal) {
     modal.style.display = "none";
   }
